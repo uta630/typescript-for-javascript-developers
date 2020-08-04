b9d4f131ab0b51cdd56eeaad0507f7d220f5b585
uta630 <usuitkc.630@gmail.com>
2020-08-04 23:40:05 +0900

    number型,string型

---
+++ src/number-string.ts
@@ -0,0 +1,12 @@
+export {};
+
+let year: number = 1976;
+// year = false; // error
+console.log(year);
+
+let age: number = 0x2b; // 2進数
+console.log(age);
+
+let name: string = 'hum';
+// name = 10; // error
+console.log(name);
